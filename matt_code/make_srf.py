8c92f1e132fbc4b16cfb33e422bb84198b5c00a1
Caleb Kumar <cekcreator@gmail.com>
2025-10-29 10:23:58 -0600

    update

--- matt_code/make_srf.py
+++ matt_code/make_srf.py
@@ -5,10 +5,9 @@ import pandas as pd
 
 
 def load_srf(channel:str, version:str = "0-0-1"):
-    parent_folder = resources.files("unfiltering") / "data" / "SRF"
-    file_name = f"libera_srf_{channel}_v{version}.csv"
+    file_name = f"./data/SRF/libera_srf_{channel}_v{version}.csv"
 
-    return pd.read_csv(parent_folder / file_name, header=1, names=["Wavelength", "Response"])
+    return pd.read_csv(file_name, header=1, names=["Wavelength", "Response"])
 
 def get_interpolated_srf(channel:str,
                          interpolation_points=np.linspace(0.3,100, 500),
